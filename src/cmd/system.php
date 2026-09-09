1f45a89a24e85b601838e7b8c85d12860559345e
Jose Izata Quinvula <joseizataquinvula885@gmail.com>
2026-09-09 01:42:17 +0100

    fix: update help command with correct command list

--- src/cmd/system.php
+++ src/cmd/system.php
@@ -13,21 +13,25 @@ function handleSystem($cmd, &$history, $userRole) {
         if ($userRole === 'root') {
             $history[] = ['out' => '[ROOT] Acesso total', 'type' => 'ok'];
             $history[] = ['out' => '  @idioma create / @idioma edit', 'type' => 'ok'];
-            $history[] = ['out' => '  pkg:create / term:add', 'type' => 'ok'];
+            $history[] = ['out' => '  @idioma download / @idioma update', 'type' => 'ok'];
         } elseif ($userRole === 'user') {
             $history[] = ['out' => '[USER] Acesso limitado', 'type' => 'skip'];
             $history[] = ['out' => '  @idioma download / @idioma update', 'type' => 'ok'];
-            $history[] = ['out' => '  pkg:list / term:find', 'type' => 'ok'];
+            $history[] = ['out' => '  term:find', 'type' => 'ok'];
         }
-        $history[] = ['out' => 'Admin:', 'type' => 'info'];
-        $history[] = ['out' => '  @login usuario senha / logout', 'type' => 'skip'];
+        $history[] = ['out' => '', 'type' => 'dim'];
         $history[] = ['out' => 'Pacotes:', 'type' => 'info'];
-        $history[] = ['out' => '  @pacotes list / @pacotes locais list', 'type' => 'ok'];
+        $history[] = ['out' => '  @pacotes list', 'type' => 'ok'];
+        $history[] = ['out' => '  @pacotes locais list', 'type' => 'ok'];
         $history[] = ['out' => '  @idioma download / @idioma update', 'type' => 'ok'];
+        $history[] = ['out' => '  @idioma create / @idioma edit (root)', 'type' => 'ok'];
+        $history[] = ['out' => '', 'type' => 'dim'];
         $history[] = ['out' => 'Traducao:', 'type' => 'info'];
         $history[] = ['out' => '  @idioma texto @idioma', 'type' => 'ok'];
+        $history[] = ['out' => '', 'type' => 'dim'];
         $history[] = ['out' => 'Sistema:', 'type' => 'info'];
-        $history[] = ['out' => '  @login usuario senha / logout / stats / clear', 'type' => 'ok'];
+        $history[] = ['out' => '  @login usuario senha / logout / @user', 'type' => 'ok'];
+        $history[] = ['out' => '  term:find / stats / clear / help', 'type' => 'ok'];
         return true;
     }
 
